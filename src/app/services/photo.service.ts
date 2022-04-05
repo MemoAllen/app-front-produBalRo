75de5b3dcb1714862888690eba935869b3ab2246
GBZ <jgbz@fsw.com.mx>
2022-04-04 20:22:48 -0500

    subida

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import {Photo} from '../interfaces/Photo'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  //URL del back para crear nuestras fotos
+  URI = 'http://localhost:4000/api/photos';
+
+  constructor(private http: HttpClient) { }
+
+
+  //Este es el metodo service para crear la foto
+  createPhoto(title: string, description: string, photo: File) {
+    const fd = new FormData();
+    fd.append('title', title);
+    fd.append('description', description);
+    fd.append('image', photo);
+    return this.http.post(this.URI, fd);
+  }
+
+  //Traer todas las fotos
+  getPhotos() {
+    return this.http.get<Photo[]>(this.URI);
+  }
+
+  getPhoto(id: string) {
+    return this.http.get<Photo>(`${this.URI}/${id}`);
+  }
+
+  deletePhoto(id: string) {
+    return this.http.delete(`${this.URI}/${id}`);
+  }
+
+  updatePhoto(id: string, title: string, description: string) {
+    return this.http.put(`${this.URI}/${id}`, {title, description});
+  }
+}
